8b6e9d888ab98e6d210dd0417603a151befd03a2
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2023-12-21 17:35:26 +0900

    Swiftでカメラ起動＆撮影

---
+++ Camera-Test/Camera_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Camera_TestApp.swift
+//  Camera-Test
+//
+//  Created by user on 2023/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Camera_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Camera-Test/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Camera-Test
+//
+//  Created by user on 2023/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isCameraPresented = false
+
+    var body: some View {
+        VStack {
+            Text("Top画面です")
+            Button(action:{
+                print("押されました")
+            })
+            {
+                Text("Pushボタン")
+            }.padding()
+            Button(action: {
+                isCameraPresented = true
+            }) {
+                Text("カメラを起動")
+            }
+            .sheet(isPresented: $isCameraPresented) {
+                CameraView()
+            }
+           
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
